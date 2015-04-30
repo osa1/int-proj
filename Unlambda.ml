e17e1dabfcfd08205f9297624a5d9c27bb75702f
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-04-29 21:00:58 -0400

    initial staged version

--- Unlambda.ml
+++ Unlambda.ml
@@ -127,15 +127,16 @@ and apply_ref (e1 : exp_s) (e2 : exp_s) (cont : cont list) : exp_s =
   | Print_S c -> print_char c; flush stdout; apply_cont e2 cont
   | E_S x -> x
   | Read_S ->
-      let c = (try Some (input_char stdin)
-               with _ -> None) in
+      let c = try Some (input_char stdin)
+              with _ -> None in
       current_char := c;
       apply_ref e2 (match c with None -> V_S | Some _ -> I_S) cont
   | Cmp_S c ->
       apply_ref e2 (if Some c = !current_char then I_S else V_S) cont
   | Repr_S ->
       apply_ref e2 (match !current_char with None -> V_S | Some c -> Print_S c) cont
-  | _ -> assert false
+  | Backtick_S _ ->
+      raise (Failure "Can't apply to backtick.")
 
 and eval_ref (exp : exp_s) (cont : cont list) =
   match exp with
@@ -144,9 +145,59 @@ and eval_ref (exp : exp_s) (cont : cont list) =
 
 (* First staged interpreter: Very powerful, but loop if programs loop *)
 
-and apply_s1 (e1 : exp_s) (e2 : exp_s) (cc : char option)
-             (cont : exp_s -> char option -> exp_s code) : exp_s code =
+let rec apply_cont_staged (e1 : exp_s) (cont : cont list) : exp_s code =
+  match cont with
+  | [] -> .< e1 >.
+  | DelayGuard e2 :: rest -> begin
+      match e1 with
+      | D_S -> apply_cont_staged (D1_S e2) rest
+      | _   -> eval_staged e2 (ApplyTo e1 :: rest)
+    end
+  | ApplyTo f :: rest -> apply_staged f e1 rest
+  | ApplyDelayed a :: rest -> apply_staged e1 a rest
+
+and apply_staged (e1 : exp_s) (e2 : exp_s) (cont : cont list) : exp_s code =
+  match e1 with
+  | K_S -> apply_cont_staged (K1_S e2) cont
+  | K1_S x -> apply_cont_staged x cont
+  | S_S -> apply_cont_staged (S1_S e2) cont
+  | S1_S x -> apply_cont_staged (S2_S (x, e2)) cont
+  | S2_S (x, y) ->
+      eval_staged (Backtick_S (Backtick_S (x, e2), Backtick_S (y, e2))) cont
+  | I_S -> apply_cont_staged e2 cont
+  | V_S -> apply_cont_staged V_S cont
+  | C_S -> apply_cont_staged (Cont_S cont) cont
+  | Cont_S cont' -> apply_cont_staged e2 cont'
+  | D_S -> apply_cont_staged e2 cont
+  | D1_S f -> eval_staged f (ApplyDelayed e2 :: cont)
+  | Print_S c -> .< let _ = print_char c in .~ (apply_cont_staged e2 cont)  >.
+  | E_S x -> .< x >.
+  | Read_S ->
+      .< let c = try Some (input_char stdin)
+                 with _ -> None in
+         current_char := c;
+         (* TODO: Explain why we can't just use `c` here *)
+         apply_ref e2 (match !current_char with None -> V_S | Some _ -> I_S) cont
+         (* .~ (apply_staged e2 (match !current_char with None -> V_S | Some _ -> I_S) cont) *)
+       >.
+  | Cmp_S c ->
+      (* TODO: we're being overly conservative here, we can have static values
+       * in current_char *)
+      .< apply_ref e2 (if Some c = !current_char then I_S else V_S) cont >.
+  | Repr_S ->
+      (* TODO: Same problem here, we may have statically known value *)
+      .< apply_ref e2 (match !current_char with None -> V_S | Some c -> Print_S c) cont >.
+  | Backtick_S _ ->
+      raise (Failure "Can't apply to backtick.")
+
+and eval_staged (exp : exp_s) (cont : cont list) : exp_s code =
+  match exp with
+  | Backtick_S (arg1, arg2) -> eval_staged arg1 (DelayGuard arg2 :: cont)
+  | _ -> apply_cont_staged exp cont
+
 (*
+let rec apply_s1 (e1 : exp_s) (e2 : exp_s) (cc : char option)
+                 (cont : exp_s -> char option -> exp_s code) : exp_s code =
   match e1 with
   | K_S -> cont (K1_S e2) cc
   | K1_S x -> cont x cc
@@ -229,7 +280,7 @@ let _ =
   let (p, pos) = parse contents in
 
   if Array.length Sys.argv == 3 && String.compare Sys.argv.(2) "stage" == 0 then
-    (* Print_code.print_code Format.std_formatter (eval_s1 (tr p) None (fun x _ -> .<x>.)) *)
+    let _ = Print_code.print_code Format.std_formatter (eval_staged (tr p) []) in
     (* Runcode.(!.) (eval_s1 (tr p) None (fun x _ -> .<x>.)); *)
     ()
   else if Array.length Sys.argv == 3 && String.compare Sys.argv.(2) "cont" == 0 then
